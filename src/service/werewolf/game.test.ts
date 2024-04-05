802f91e6e47dc7550b92ff2e0a007d3a9510c031
Pong <samfunghp@gmail.com>
2024-04-05 17:14:14 +0100

    fix(): complete basic game flow

--- src/service/werewolf/game.test.ts
+++ src/service/werewolf/game.test.ts
@@ -1,37 +1,38 @@
 import { test, expect } from 'vitest';
 import { Game } from './game';
 import { Character, Villager, Werewolf } from './character';
-import { Init, Start, Night, Daytime, Stage } from './stage';
+import { Init, Start, Night, Daytime, Stage, End } from './stage';
 import { errors } from './error';
 import { Voted } from './death';
 
-const testSerialisation = (game: Game) => {
+let game: Game = Game.create({ id: '1' });
+let stage: Stage = game.stage;
+let survivors: Character[] = [];
+let werewolfs: Werewolf[] = [];
+let villagers: Villager[] = [];
+
+const testSerialisation = () => {
   const serialized = Game.create(Game.serialize(game));
   expect(game).toMatchObject(serialized);
 };
 
-test('flow', () => {
-  const game = Game.create({ id: '1' });
+const next = (StageConstructor: typeof Stage) => {
+  stage = game.next();
+  testSerialisation();
+  expect(stage).toBeInstanceOf(StageConstructor);
 
-  let stage = game.stage;
-  let survivors: Character[] = [];
-  let werewolfs: Werewolf[] = [];
-  let villagers: Villager[] = [];
+  survivors = game.stage.survivors;
+  werewolfs = game.getCharacters(Werewolf);
+  villagers = game.getCharacters(Villager);
+};
 
-  const next = (StageConstructor: typeof Stage) => {
-    stage = game.next();
-    testSerialisation(game);
-    expect(stage).toBeInstanceOf(StageConstructor);
-
-    survivors = game.stage.survivors;
-    werewolfs = game.getCharacters(Werewolf);
-    villagers = game.getCharacters(Villager);
-  };
+const createGame = ({ numOfPlayers = 10 } = {}) => {
+  game = Game.create({ id: '1' });
+  stage = game.stage;
 
   expect(stage).toBeInstanceOf(Init);
-  testSerialisation(game);
-
-  // --------------------------------------------------------------------------------
+  stage.numOfPlayers = numOfPlayers;
+  testSerialisation();
 
   next(Start);
   expect(() => game.next()).toThrowError(errors('NOT_ENOUGH_PLAYERS'));
@@ -47,8 +48,11 @@ test('flow', () => {
     }
   }
   expect(game.players.size).toEqual(game.stage.numOfPlayers);
+};
 
-  // --------------------------------------------------------------------------------
+test('flow', () => {
+  createGame({ numOfPlayers: 6 });
+  expect(game.stage.numOfPlayers).toBe(6);
 
   next(Night);
   expect(stage.turn).toBe(1);
@@ -89,32 +93,25 @@ test('flow', () => {
   // --------------------------------------------------------------------------------
 
   // everyone gets one vote in previous round, so vote again
-  next(Daytime);
-  daytime = stage.as(Daytime);
-  expect(daytime.candidates.size).toBe(survivors.length);
-  expect(() => game.next()).toThrowError(errors('STAGE_NOT_ENDED'));
-  expect(() => villagers[2].vote(villagers[0])).toThrowError(errors('TARGET_IS_DEAD')); // expecet not to VOTE_OUT_OF_RANGE
+  for (let i = 0; i < 10; i++) {
+    next(Daytime);
+    daytime = stage.as(Daytime);
 
-  villagers[1].vote(werewolfs[0]);
-  werewolfs[0].vote(villagers[1]);
-  survivors.forEach(survivor => !survivor.endTurn && survivor.waive());
+    if (i === 0) {
+      expect(daytime.candidates.size).toBe(survivors.length);
+    } else {
+      expect(daytime.candidates.size).toBe(2);
+    }
 
-  expect(daytime.countResults()).toMatchObject({ numberOfVotes: 2, count: 1 });
+    expect(() => game.next()).toThrowError(errors('STAGE_NOT_ENDED'));
+    expect(() => villagers[2].vote(villagers[0])).toThrowError(errors('TARGET_IS_DEAD')); // expecet not to VOTE_OUT_OF_RANGE
 
-  // --------------------------------------------------------------------------------
+    villagers[1].vote(werewolfs[0]);
+    werewolfs[0].vote(villagers[1]);
+    survivors.forEach(survivor => !survivor.endTurn && survivor.waive());
 
-  // two players gets one vote, others player wavied, so vote again
-  next(Daytime);
-  daytime = stage.as(Daytime);
-  expect(daytime.candidates.size).toBe(2);
-  expect(() => villagers[2].vote(villagers[2])).toThrowError(errors('VOTE_OUT_OF_RANGE'));
-  expect(() => villagers[2].vote(villagers[3])).toThrowError(errors('VOTE_OUT_OF_RANGE'));
-
-  villagers[1].vote(werewolfs[0]);
-  werewolfs[0].vote(villagers[1]);
-  survivors.forEach(survivor => !survivor.endTurn && survivor.waive());
-
-  expect(daytime.countResults()).toMatchObject({ numberOfVotes: 2, count: 1 });
+    expect(daytime.countResults()).toMatchObject({ numberOfVotes: 2, count: 1 });
+  }
 
   // --------------------------------------------------------------------------------
 
@@ -127,10 +124,27 @@ test('flow', () => {
 
   // --------------------------------------------------------------------------------
 
-  // villagers[1] is dead as all players voted
+  // villagers[1] is dead by voting
 
   next(Night);
   expect(villagers[1].causeOfDeath[0]).toBeInstanceOf(Voted);
   expect(villagers[1].causeOfDeath[0] as Voted).toHaveProperty('total', survivors.length);
   expect(survivors).toHaveLength(stage.numOfPlayers - 2);
+
+  expect(() => werewolfs[0].kill(villagers[1])).toThrowError(errors('TARGET_IS_DEAD')); // expecet not to VOTE_OUT_OF_RANGE
+  werewolfs[0].kill(villagers[2]);
+
+  // --------------------------------------------------------------------------------
+
+  // villagers[2] is killed by werewolf
+
+  next(Daytime);
+  expect(survivors).toHaveLength(3);
+  werewolfs[0].vote(villagers[3]);
+  villagers[4].vote(villagers[3]);
+  villagers[3].vote(werewolfs[0]);
+
+  // --------------------------------------------------------------------------------
+
+  next(End);
 });

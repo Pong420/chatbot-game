114fb68a0601ba9fda1fcf394199fa20b69ba835
Pong <samfunghp@gmail.com>
2024-04-10 07:52:37 +0100

    feat(): throw error to supabase api

--- src/supabase/game.ts
+++ src/supabase/game.ts
@@ -4,13 +4,13 @@ import { Tables, TablesInsert, TablesUpdate } from './database.types';
 export type Game = Tables<'games'>;
 
 export function getGame(groupId: string) {
-  return supabase.from('games').select('*').eq('groupId', groupId).single();
+  return supabase.from('games').select('*').eq('groupId', groupId).single().throwOnError();
 }
 
 export function createGame(data: TablesInsert<'games'>) {
-  return supabase.from('games').insert([data]).select().single();
+  return supabase.from('games').insert([data]).select().single().throwOnError();
 }
 
 export function updateGame(groupId: string, data: TablesUpdate<'games'>) {
-  return supabase.from('games').update(data).eq('groupId', groupId).select().single();
+  return supabase.from('games').update(data).eq('groupId', groupId).select().single().throwOnError();
 }
--- src/supabase/user.ts
+++ src/supabase/user.ts
@@ -4,13 +4,13 @@ import { Tables, TablesInsert, TablesUpdate } from './database.types';
 export type User = Tables<'users'>;
 
 export function getUser(userId: string) {
-  return supabase.from('users').select('*').eq('userId', userId).single();
+  return supabase.from('users').select('*').eq('userId', userId).single().throwOnError();
 }
 
 export function createUser(data: TablesInsert<'users'>) {
-  return supabase.from('users').insert([data]).select().single();
+  return supabase.from('users').insert([data]).select().single().throwOnError();
 }
 
 export function updateUser(userId: string, data: TablesUpdate<'users'>) {
-  return supabase.from('users').update(data).eq('userId', userId).select().single();
+  return supabase.from('users').update(data).eq('userId', userId).select().single().throwOnError();
 }
